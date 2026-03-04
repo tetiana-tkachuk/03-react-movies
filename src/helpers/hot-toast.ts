1a24a5f7fdcc36e28d11e72278ba4a0cd53c32f0
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-04 11:34:31 +0200

    Update hot-toast

--- src/helpers/hot-toast.ts
+++ src/helpers/hot-toast.ts
@@ -1,11 +1,11 @@
 import toast from 'react-hot-toast';
 
-export const notify = (message: string) => toast(message);
+export const notify = (message: string) => toast.error(message);
 
 export const toastOptions = {
   className: '',
   style: {
-    border: '1px solid #0a66c2',
+    // border: '1px solid #ccc',
     padding: '16px',
     color: '#0a66c2',
   },
